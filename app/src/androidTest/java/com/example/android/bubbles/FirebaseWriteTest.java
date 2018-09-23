4317c7c9fd13387a10313543d4ea77efa3cb05c7
Piotr Galusza <piotrjg2@linux-17.ews.illinois.edu>
2018-09-23 01:14:03 -0500

    Removed example code as cleanup

--- app/src/androidTest/java/com/example/android/bubbles/FirebaseWriteTest.java
+++ app/src/androidTest/java/com/example/android/bubbles/FirebaseWriteTest.java
@@ -34,3 +34,11 @@ public class FirebaseWriteTest {
         writeSignal.await(10, TimeUnit.SECONDS);
     }
 }
+
+// @Test
+// public void useAppContext() throws Exception {
+//     // Context of the app under test.
+//     Context appContext = InstrumentationRegistry.getTargetContext();
+//
+//     assertEquals("com.example.android.bubbles", appContext.getPackageName());
+// }
